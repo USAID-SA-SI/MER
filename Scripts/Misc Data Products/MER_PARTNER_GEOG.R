84fb7e345a4352e2b4213e4f7f10e34d750e6176
Gina Sarfaty <gsarfaty@usaid.gov>
2022-12-15 09:20:32 -0500

    initial script to pull unique geog for each partner

---
+++ Scripts/Misc Data Products/MER_PARTNER_GEOG.R
@@ -0,0 +1,39 @@
+library(tidyverse)
+library(readxl)
+library(gophr)
+library(here)
+library(glamr)
+
+memory.limit(size=500000)
+
+
+current_pd<-"FY22Q4i"
+
+
+
+#genie 
+genie_files<-list.files(here("Data/site"),pattern="SITE")
+
+genie<-here("Data/site",genie_files) %>% 
+  map(read_msd, save_rds=FALSE, remove_txt = FALSE) %>% 
+  reduce(rbind) %>%
+  filter(fiscal_year %in% c("2022"),
+         funding_agency=="USAID")
+
+  
+geog_df<-genie %>% 
+  distinct(orgunituid,psnu,psnuuid,
+           community,communityuid,facility,facilityuid,sitename,sitetype,mech_code,prime_partner_name) %>% 
+  relocate(sitetype, .before=sitename) %>% 
+  relocate(community, .after=psnuuid) %>% 
+  relocate(facility, .after=community) %>% 
+  relocate(communityuid, .after=community) %>% 
+  relocate(facilityuid, .after=facility)
+
+
+  
+# Dataout ----------------------------------------------------------------------
+
+filename<-paste(Sys.Date(),"PEPFAR_SD_PARTNER_GEOG",current_pd,".txt",sep="_")
+
+write_tsv(geog_df, file.path(here("Dataout"),filename,na=""))
