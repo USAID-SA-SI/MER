4c8312703e206272c0a7cc8daee04d42118ecf80
Gina Sarfaty <gsarfaty@usaid.gov>
2022-12-19 10:24:55 -0500

    remove above site reporting & HRH indicators

--- Scripts/Misc Data Products/MER_PARTNER_GEOG.R
+++ Scripts/Misc Data Products/MER_PARTNER_GEOG.R
@@ -4,8 +4,6 @@ library(gophr)
 library(here)
 library(glamr)
 
-memory.limit(size=500000)
-
 
 current_pd<-"FY22Q4i"
 
@@ -18,7 +16,9 @@ genie<-here("Data/site",genie_files) %>%
   map(read_msd, save_rds=FALSE, remove_txt = FALSE) %>% 
   reduce(rbind) %>%
   filter(fiscal_year %in% c("2022"),
-         funding_agency=="USAID")
+         funding_agency=="USAID",
+         !sitetype=="Above Site",
+         !indicator %in% c("HRH_STAFF_NAT","HRH_PRE"))
 
   
 geog_df<-genie %>% 
@@ -31,6 +31,13 @@ geog_df<-genie %>%
   relocate(facilityuid, .after=facility)
 
 
+# DATA CHECK 
+check_dsp<-geog_df %>% 
+  filter(mech_code %in% c("70290")) %>% 
+  group_by(prime_partner_name,psnu,sitetype) %>% 
+  count() %>% 
+  print()
+  
   
 # Dataout ----------------------------------------------------------------------
 
